fc0f4f135b8affa5ed3cd23ca8f55f1f5ac2d3fe
a2564011261 <a2564011261@163.com>
2021-11-14 10:10:00 +0800

    update

--- import os.py
+++ import os.py
@@ -7,7 +7,26 @@ source = []
 source_searched = []
 theme = []
 theme_searched = []
+# laoding
+text = ''
 for i in zhailu1:
     if i != "|":
         text = text + i
-    elif i == '|'
+    elif i == '|':
+        if text == 'sentence':
+            a = 1
+            text = ''
+        elif text == 'source':
+            a = 2
+            text = ''
+        elif text == 'theme':
+            a = 3
+            text = ''
+        if a == 1:
+            sentence.append(text)
+        elif a == 2:
+            source.append(text)
+        elif a == 3:
+            theme.append(text)
+# menu
+event = input('[1]添加摘录\n[2]查询摘录\n请选择:')
