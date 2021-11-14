4726ba19241704104f68c2b0509a87a5af52deed
a2564011261 <a2564011261@163.com>
2021-11-14 10:34:16 +0800

    add ADD part

--- import os.py
+++ import os.py
@@ -1,12 +1,40 @@
-import os
-zhailu = open('zhailu1.file',w)
-zhailu1 = open('zhailu.file',r)
+import os,sys
+zhailu = open('zhailu1.file','w')
+zhailu1 = open('zhailu.file','r')
 sentence = []
 sentence_searched = []
 source = []
 source_searched = []
 theme = []
 theme_searched = []
+def Search():
+    keyword = input('请输入句子关键词：')
+    for i in sentence:
+        if keyword in i:
+            sentence_searched
+def ADD():
+    word_sentence = input('[句子]')
+    if word_sentence == '':
+        print('[error]未填写必填项')
+    else:
+        sentence.append(word_sentence)
+    word_source = input('[来源]')
+    if word_source == '':
+        source.append('')
+    else:
+        source.append(word_source)
+    word_theme = input('[适用主题]')
+    if word_theme == '':
+        theme.append('')
+    else:
+        theme.append(word_theme)
+    a = sentence.index(word_sentence)
+    if source[a] == word_source and theme[a] == word_theme:
+        print('已成功录入')
+    else:
+        print('[error]程序错误')
+        os.system('pause')
+        sys.exit()
 # laoding
 text = ''
 for i in zhailu1:
@@ -30,3 +58,7 @@ for i in zhailu1:
             theme.append(text)
 # menu
 event = input('[1]添加摘录\n[2]查询摘录\n请选择:')
+if event == '1':
+    ADD()
+elif event == '2':
+    pass
