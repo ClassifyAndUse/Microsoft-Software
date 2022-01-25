b0853ea9f24f3f93831847ee7483915a2c228d71
a2564011261 <a2564011261@163.com>
2022-01-25 11:38:29 +0800

    update

---
+++ setup.py
@@ -0,0 +1,20 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+from setuptools import setup
+
+setup(
+    name='excerpt_tool', # 项目的名称,pip3 install get-time
+    version='0.0.1', # 项目版本 
+    author='Mryan2005', # 项目作者 
+    url='https://github.com/Mryan2005/excerpt_tool', # 项目代码仓库
+    description='获取任意时间/获取当前的时间戳/时间转时间戳/时间戳转时间', # 项目描述 
+    packages=['excerpt_tool'], # 包名 
+    install_requires=[],
+    entry_points={
+        'console_scripts': [
+            'excerpte=excerpt_tool:main', # 使用者使用excerpt时,就睡到excerpt项目下的__init__.py下执行excerpt函
+        ]
+    } # 重点
+)
+
