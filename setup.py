3bf541aa4cde06b056a59632f64ec5855bb65af0
a2564011261 <a2564011261@163.com>
2022-01-25 12:27:29 +0800

    update ver

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ from setuptools import setup
 
 setup(
     name='excerpt_tool', # 项目的名称,pip3 install get-time
-    version='0.0.2', # 项目版本 
+    version='0.0.3', # 项目版本 
     author='Mryan2005', # 项目作者 
     url='https://github.com/Mryan2005/excerpt_tool', # 项目代码仓库
     description='一个摘录软件', # 项目描述 
